1ca4039edb4fbad24acee50518660f1a9d70bd3c
Antyss77 <fragnatiikpvpbest@gmail.com>
2023-03-08 19:09:04 +0100

    Initial commit

---
+++ Question.cs
@@ -0,0 +1,9 @@
+﻿class Question {
+    public string Text { get; set; }
+    public string[] Reponses { get; set; }
+    public string ReponseCorrecte { get; set; }
+
+    public bool VerifierReponse(string reponse) {
+        return reponse == ReponseCorrecte;
+    }
+}
